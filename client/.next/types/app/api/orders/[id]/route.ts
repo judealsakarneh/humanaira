b8b9955dce3a8347ba108d99e9f7414175416cfb
judealsakarneh <judealsakarneh@gmail.com>
2025-09-07 03:06:24 +0300

    Rename empty orders [id] API route to fix build

--- client/.next/types/app/api/orders/[id]/route.ts
+++ client/.next/types/app/api/orders/[id]/route.ts
@@ -1,8 +1,8 @@
 // File: C:\Users\JUDE\Desktop\ZENTASK-AI\client\src\app\api\orders\[id]\route.ts
-import * as entry from '../../../../../../src/app/api/orders/[id]/route.js'
+import * as entry from '../../../../../../src/app/api/orders/[id]/route.ts.bak'
 import type { NextRequest } from 'next/server.js'
 
-type TEntry = typeof import('../../../../../../src/app/api/orders/[id]/route.js')
+type TEntry = typeof import('../../../../../../src/app/api/orders/[id]/route.ts.bak')
 
 type SegmentParams<T extends Object = any> = T extends Record<string, any>
   ? { [K in keyof T]: T[K] extends string ? string | string[] | undefined : never }
