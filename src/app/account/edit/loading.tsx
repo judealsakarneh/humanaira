280a97506178bb7e991e17faf809dc1e1a001632
judealsakarneh <judealsakarneh@gmail.com>
2025-12-10 20:36:09 +0300

    Fix: Move PremiumLoader to correct location and update import path

--- src/app/account/edit/loading.tsx
+++ src/app/account/edit/loading.tsx
@@ -1,17 +1,5 @@
-'use client'
-
-const BRAND = '#35BFFF'
+import PremiumLoader from '../../../components/PremiumLoader'
 
 export default function Loading() {
-  return (
-    <div className="min-h-screen flex flex-col items-center justify-center bg-slate-950 text-white p-4">
-      <div
-        className="w-12 h-12 border-4 border-solid border-t-transparent rounded-full animate-spin mb-4"
-        style={{ borderColor: BRAND }}
-      />
-      <div className="text-xl font-medium" style={{ color: BRAND }}>
-        Loading editor...
-      </div>
-    </div>
-  )
+  return <PremiumLoader text="Loading editor..." />
 }
